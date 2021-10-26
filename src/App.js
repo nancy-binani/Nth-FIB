5cee566b263f56bc41ae8e0084130439ab491e18
Nancy Binani <nancybinani1207@gmail.com>
2021-10-26 16:05:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Fibo from './Fibo'
+import './Fibo.css'
+function App() {
+  return (
+    <div>
+       <h2>Fibonacci Calculator:-</h2>
+    <div className="container">
+      
+      <Fibo/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Fibo.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import './Fibo.css'
+const Fibo = (e) => {
+    
+    let [taskData,setTaskData] = useState("");
+    const [total, setTotal] = useState();
+  
+    function fibonacci() {
+  
+        let a=0,b=1,c=0;
+        
+        for(let i = 2; i <= taskData; i++) {
+          c=a+b;
+          a=b;
+          b=c;
+        }
+        
+        setTotal(a);
+        setTaskData("")
+      }
+    
+    return(
+    <div>
+       
+          <input
+          type="number"
+          value={taskData}
+          onChange={e => setTaskData(e.target.value)}
+          
+        />
+        <button className="btn" onClick={fibonacci}> submit </button>
+        <h2>Value:- {total}</h2>
+    </div>
+    )
+}
+
+export default Fibo;
